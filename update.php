ff2eb0cb60d9035b21375959ef42e841f9f33074
DhruvTyagi <55016553+dhruvtyagi98@users.noreply.github.com>
2020-09-29 12:03:57 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title></title>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><b>STUDENT RECORDS</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="home.php">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="update.php">Update<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="store.php">Store</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      <?php
+      if (isset($_GET['Login'])) {
+        if ($_GET['Login'] =="Success") {
+       ?>
+      Welcome,<?=$_SESSION['username']?>
+      <?php
+    }
+  }
+       ?>
+    </span>
+  </div>
+</nav>
+  </head>
+  <body>
+
+  </body>
+</html>
